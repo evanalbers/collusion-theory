6a27ecd1ff1018a7db93099eeb3bc1d9028e4a25
Evan Albers <evaers@CSL16630.local>
2025-11-24 13:21:13 +0000

    added maxe submodule

---
+++ agent.py
@@ -0,0 +1,67 @@
+from thesimulator import *
+
+def policy():
+    """ Agent policy that determines the price the agent proposes
+        and the fraction of capital the agent invests at time t
+    
+    Params
+    ------
+    v_t_prime : float
+        market signal v prime at time t
+
+    Returns
+    -------
+    price_t : float
+        price agent will charge 
+
+    k_t : float
+        proportion of total capital invested in production            
+    """
+
+def calculate_capital_investment():
+    """ Calculates agent capital investment at time t"""
+    pass
+
+def calculate_capital_update():
+    """ Calculates the agent's capital for the next timestep based
+        on its own sales at its own price. 
+        
+        Params
+        ------
+        k_t : float
+            proportion of capital invested
+        
+        R_t_m1 : float
+            capital at timestep t-1
+            
+        p_t : float
+            price proposed at timestep t
+
+        v_i : int
+            volume of goods sold at timestep t
+
+        """
+
+
+
+class ProducerAgent: 
+
+    production_function = None
+    policy = None
+
+
+    """ Representing the behavior of the producer agent described in the Simulation 
+    Agent Attributes: 
+
+
+    
+    
+    
+    """
+
+    def configure(self, params):
+        """ saving parameters to agent object """
+        pass
+    
+    def receiveMessage(self, simulation, type, payload):
+        """ agent behavior is defined relative to messaged received. """
---
+++ market.py
@@ -0,0 +1,46 @@
+from thesimulator import *
+import numpy as np
+
+def calculate_market_share(agent, prices, softness):
+    """ calculates the market share of the given agent based on the given prices 
+    Params
+    ------
+    agent : int
+        index represent the id of the agent
+    
+    prices : np.array
+        array of prices submitted by each agent
+    
+    softness : float
+        representing how "soft" the market is as defined in paper (lambda)
+
+    Returns
+    -------
+    share : float
+        market share of given agent 
+
+    """
+
+    numerator = np.exp(-prices[agent] / softness)
+
+    denom = 0
+    for a in prices:
+        denom += np.exp(-prices[a] / softness)
+
+    return numerator / denom
+
+def resolve_transactions():
+    """ Transaction resolution for each agent in """
+
+
+class MarketAgent: 
+    """ Agent class representing the market where ProducerAgents transact
+
+        Basic pattern is gather all price estimates from the market, process them, send back 
+        quantities demanded. 
+      
+    """
+
+    softness = 0
+
+    
