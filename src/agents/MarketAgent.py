7ad28238b30c24533af1848be5ee21cae3168258
Evan Albers <evaers@CSL16630.local>
2025-11-29 17:23:09 +0000

    updating everything

--- src/agents/MarketAgent.py
+++ src/agents/MarketAgent.py
@@ -1,9 +1,37 @@
 from thesimulator import *
 import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+
+def market_demand(price):
+    """ returns volume demanded """
+
+    return price / 5
+
+def mean_function(price, quantity, scaling_factor, price_elas):
+    """ returns the mean of the GP representing market context """
+
+    return scaling_factor * quantity * price ** (-price_elas)
 
 
+def sample_market_context(scaling_factor, quantity, price, price_elas):
+    """ Samples the market context for a given quantity and price """
 
-def calculate_market_share(agent, prices, softness):
+    # defining the kernel process
+    kernel = RBF(length_scale=1.0)
+    gp = GaussianProcessRegressor(kernel=kernel)
+
+    # sampling GP to get mu, phi
+    mu, phi = gp.predict([[price, quantity]], return_std=True)
+
+    # calc prior relating mu to price, quantity
+    prior = mean_function(price, quantity, scaling_factor, price_elas)
+
+    sample_mu = prior + mu
+
+    return np.random.normal(sample_mu, phi)
+
+def calculate_market_share(num_agents, prices, softness):
     """ calculates the market share of the given agent based on the given prices 
     Params
     ------
@@ -22,18 +50,41 @@ def calculate_market_share(agent, prices, softness):
         market share of given agent 
 
     """
+    shares = np.zeros(num_agents)
 
-    numerator = np.exp(-prices[agent] / softness)
+    for agent in num_agents:
 
-    denom = 0
-    for a in prices:
-        denom += np.exp(-prices[a] / softness)
+        numerator = np.exp(-prices[agent] / softness)
 
-    return numerator / denom
+        denom = 0
+        for a in prices:
+            denom += np.exp(-prices[a] / softness)
 
-def resolve_transactions():
+        shares[agent] = numerator / denom
+
+    return shares
+
+def resolve_transactions(prices, offers, softness, demand_function):
     """ Transaction resolution for each agent in """
 
+    winning_price = np.min(prices)
+
+    market_demand = demand_function(winning_price)
+
+    if softness == 0:
+        shares = np.zeros(prices.size)
+        winners = np.where(prices == winning_price)
+
+        winning_shares = 1 / len(winners)
+
+        shares[winners] = winning_shares
+
+    else:
+        shares = calculate_market_share(prices.size, prices, softness)
+
+    return np.minimum(shares * market_demand, offers)
+
+    
 class MarketAgent: 
     """ Agent class representing the market where ProducerAgents transact
 
@@ -41,15 +92,80 @@ class MarketAgent:
         quantities demanded. 
       
     """
-
     softness = 0
+    num_agents = 0
+    scaling_factor = 1
+    price_elas = 1
+    timestep_volume_bids = {}
+    
     
     def configure(self, params):
         """ Configures the Market Agent with corresponding params"""
-
+        print(params)
         self.softness = float(params['softness'])
-
-        print("Softness of the market: " + str(self.softness))
+        self.num_agents = int(params['num_agents'])
+        self.scaling_factor = float(params['scaling_factor'])
+        self.price_elas = float(params['price_elas'])
+        self.sim_duration = int(params['duration'])
+        self.market_contexts = np.zeros(self.sim_duration)
+        self.init_quantity = int(params['initial_quantity'])
+        self.init_price = int(params['initial_price'])
 
     def receiveMessage(self, simulation, type, payload, source):
         """ agent behavior is defined relative to messaged received. """
+
+        current_timestamp = simulation.currentTimestamp()
+
+        # Three possible types of message:
+
+        # One: Simulation start - in this case, start getting bids for first timestep. 
+        if type == "EVENT_SIMULATION_START":
+            market_context = sample_market_context(self.scaling_factor, self.init_quantity, self.init_price, self.price_elas)
+            self.market_contexts[current_timestamp] = market_context
+            for agent in range(self.num_agents):
+                print("dispatched signal to agent " + str(agent))
+                simulation.dispatchGenericMessage(current_timestamp, 1, self.name(), str(agent), "MARKET_SIGNAL", {"v_prime" : str(market_context)})
+
+        # Two: bid from an agent 
+            # two subcases here: in first, don't have all orders, in second we do. 
+            # in first, add the information then sleep
+            # in second, we resolve transactions on the timestep and then send out info
+
+        if type == "BID":
+
+            self.timestep_volume_bids[source] = (payload['price'], payload['offer'])
+
+            # if we have all the bids, need to resolve transactions
+            if len(self.timestep_volume_bids.keys()) == self.num_agents:
+
+                # create prices, volumes array 
+                prices = np.zeros(self.num_agents)
+                offers = np.zeros(self.num_agents)
+
+                for key in self.timestep_volume_bids.keys():
+                    prices[int(key)] = self.timestep_volume_bids[key][0]
+                    offers[int(key)] = self.timestep_volume_bids[key][1]
+
+                
+                realized_volumes = resolve_transactions(prices, offers, self.softness, market_demand)
+
+                # send out realized volumes 
+                for agent in range(self.num_agents):
+                    simulation.dispatchGenericMessage(current_timestamp, 0, self.name(), str(agent), "RESOLVED_TRANSACTION", {"realized_vol" : str(realized_volumes[agent])})
+
+                # zero out timestep bids for next timestep
+                self.timestep_volume_bids = {}
+            
+                market_context = sample_market_context(self.scaling_factor, self.init_quantity, self.init_price, self.price_elas)
+                self.market_contexts[current_timestamp] = market_context
+                for agent in range(self.num_agents):
+                    print("dispatched signal to agent " + str(agent))
+                    simulation.dispatchGenericMessage(current_timestamp, 1, self.name(), str(agent), "MARKET_SIGNAL", {"v_prime" : str(market_context)})
+
+        #NEED TO HAVE WAKEUP MESSAGE THAT TRIGGERS THIS ALL AGAIN - RIGHT NOW SENDS OUT 4 FOR 
+
+        if type == "EVENT_SIMULATION_STOP":
+            pass
+
+        # Three: simulation is over. write out data and quit.
+
--- src/agents/ProducerAgent.py
+++ src/agents/ProducerAgent.py
@@ -1,6 +1,13 @@
 from thesimulator import *
+import numpy as np
+import h5py
 
-def policy():
+def production_function(k, r):
+    """ returns quantity of product produced as function of capital """
+
+    return k * r * 1.5 
+
+def policy(market_context):
     """ Agent policy that determines the price the agent proposes
         and the fraction of capital the agent invests at time t
     
@@ -18,11 +25,10 @@ def policy():
         proportion of total capital invested in production            
     """
 
-def calculate_capital_investment():
-    """ Calculates agent capital investment at time t"""
-    pass
+    return 10, 0.5
 
-def calculate_capital_update():
+
+def calculate_capital_update(k_t, R_t_m1, p_t, v_i):
     """ Calculates the agent's capital for the next timestep based
         on its own sales at its own price. 
         
@@ -41,27 +47,75 @@ def calculate_capital_update():
             volume of goods sold at timestep t
 
         """
+    
+    return (1 - k_t) * R_t_m1 + p_t * v_i
 
 
 
 class ProducerAgent: 
 
-    production_function = None
-    policy = None
+    # production_function = basic_production_function
+    policy = policy
 
 
     """ Representing the behavior of the producer agent described in the Simulation 
     Agent Attributes: 
-
-
-    
-    
     
     """
 
     def configure(self, params):
         """ saving parameters to agent object """
-        pass
+        self.sim_duration = int(params['duration'])
+        self.k_values = np.zeros(self.sim_duration)
+        self.prices = np.zeros(self.sim_duration)
+        self.capital = np.zeros(self.sim_duration)
+        self.capital[0] = params['endowment']
+        self.output_fp = params['output_fp']
+
     
     def receiveMessage(self, simulation, type, payload, source):
         """ agent behavior is defined relative to messaged received. """
+
+        # Four possible messages: 
+        # Simulation start: go to sleep wait for request from market for timestep 1
+        # Market signal: context signal from the market to send a bid 
+        # Resolved transaction: update capital, go to sleep and wait for request for next timestep
+        # Simulation end: write out data, quit etc. 
+
+        current_timestamp = simulation.currentTimestamp()
+
+        if type == "EVENT_SIMULATION_START":
+            print(self.name())
+            # Just sleep here, I think
+            pass
+        elif type == "MARKET_SIGNAL":
+            v_prime = payload["v_prime"][0]
+            self.prices[current_timestamp], self.k_values[current_timestamp] = policy(v_prime)
+            offer = production_function(self.k_values[current_timestamp], self.capital[current_timestamp-1])
+            print("Name "+ self.name())
+            print("Offer " + str(offer))
+            print("Prices " + str(self.prices[current_timestamp]))
+            print("Timestamp " + str(current_timestamp))
+            
+            simulation.dispatchGenericMessage(current_timestamp, 0, self.name(), "Market", "BID", {"price" : str(self.prices[current_timestamp]), "offer" : str(offer)})
+
+        elif type == "RESOLVED_TRANSACTION":
+
+            volume_sold = payload["realized_vol"]
+            self.capital[current_timestamp] = (1 - self.k_values[current_timestamp]) * self.capital[current_timestamp - 1] + self.prices[current_timestamp] * float(volume_sold)
+            # Then sleep
+            
+            pass
+        elif type == "EVENT_SIMULATION_STOP":
+            
+            with h5py.File(self.output_fp + '/' + self.name() + '.h5', 'w') as f:
+                f.create_dataset("prices", data=self.prices)
+                f.create_dataset("capital", data=self.capital)
+                f.create_dataset("k_values", data=self.k_values)
+            
+        
+
+        else:
+            print("Unknown message type recieved!")
+        
+        
